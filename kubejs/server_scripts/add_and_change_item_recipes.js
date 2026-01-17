69b95d3f59adb6ad73fafc06274dc6c5402d3115
MiniOozy <nateteep5@gmail.com>
2026-01-17 01:00:57 -0600

    Changed recipes, added mods, and created quests.
    See changelog.

--- kubejs/server_scripts/add_and_change_item_recipes.js
+++ kubejs/server_scripts/add_and_change_item_recipes.js
@@ -1,36 +1,50 @@
-/* 
+/*
  * ServerEvents.recipes(callback) is a function that accepts another function,
- * called the "callback", as a parameter. The callback gets run when the 
+ * called the "callback", as a parameter. The callback gets run when the
  * server is working on recipes, and then we can make our own changes.
- * When the callback runs, it is also known as the event "firing". 
-*/
+ * When the callback runs, it is also known as the event "firing".
+ */
 
 // Listen for the "recipes" server event.
-ServerEvents.recipes(event => {
+ServerEvents.recipes((event) => {
   // You can replace `event` with any name you like, as
   // long as you change it inside the callback too!
-  // MACE REPLACEMENT 
-  event.replaceInput({input: 'minecraft:mace' },'minecraft:mace','oritech:electric_mace')
+  // MACE REPLACEMENT
+  event.replaceInput(
+    { input: "minecraft:mace" },
+    "minecraft:mace",
+    "oritech:electric_mace",
+  );
   event.shaped(
-  Item.of('fargostalismans:nether_star_talisman', 1), // arg 1: output
-  [
-    'ABA',
-    'DCE', // arg 2: the shape (array of strings)
-    'AFA'
-  ],
-  {
-    A: 'minecraft:nether_star',
-    B: 'minecraft:mace',  //arg 3: the mapping object
-    C: 'fargostalismans:talisman_base_epic',
-    D: 'minecraft:netherite_sword',
-    E: 'minecraft:netherite_axe',
-    F: 'minecraft:trident'
-  }
-)
+    Item.of("fargostalismans:nether_star_talisman", 1), // arg 1: output
+    [
+      "ABA",
+      "DCE", // arg 2: the shape (array of strings)
+      "AFA",
+    ],
+    {
+      A: "minecraft:nether_star",
+      B: "minecraft:mace", //arg 3: the mapping object
+      C: "fargostalismans:talisman_base_epic",
+      D: "minecraft:netherite_sword",
+      E: "minecraft:netherite_axe",
+      F: "minecraft:trident",
+    },
+  );
+  event.recipes.ars_nouveau.imbuement(
+    "oritech:heisenberg_compensator", // input item
+    "oritech:prometheum_ingot", // output
+    "10000", // source cost
+    [
+      "oritech:overcharged_crystal",
+      "oritech:overcharged_crystal",
+      "irons_spellbooks:arcane_ingot",
+    ], // pedestal items
+  );
 
   // This part, inside the curly braces, is the callback.
   // You can modify as many recipes as you like in here,
   // without needing to use ServerEvents.recipes() again.
 
-  console.log('Hello! The recipe event has fired!')
-})
+  console.log("Hello! The recipe event has fired!");
+});
